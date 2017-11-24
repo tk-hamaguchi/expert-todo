5a65c96527a26d99ab602cb741457aea03090c4c
Takahiro HAMAGUCHI <tk.hamaguchi@gmail.com>
2017-11-24 11:13:15 +0900

    Modify CicleCI config.

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -8,6 +8,9 @@ cat > ~/.netrc << EOF
 machine api.heroku.com
   login $HEROKU_LOGIN
   password $HEROKU_API_KEY
+machine git.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
 EOF
 
 cat >> ~/.ssh/config << EOF
