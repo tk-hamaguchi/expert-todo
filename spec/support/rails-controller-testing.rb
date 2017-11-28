3ffc5ab5d3ae825a041145f936bf43b197c3b108
Takahiro HAMAGUCHI <tk.hamaguchi@gmail.com>
2017-11-28 22:18:50 +0900

    Remove rails-controller-testing

--- spec/support/rails-controller-testing.rb
+++
@@ -1,8 +0,0 @@
-RSpec.configure do |config|
-  require 'rails-controller-testing'
-  %i[controller view request].each do |type|
-    config.include ::Rails::Controller::Testing::TestProcess,        type: type
-    config.include ::Rails::Controller::Testing::TemplateAssertions, type: type
-    config.include ::Rails::Controller::Testing::Integration,        type: type
-  end
-end
