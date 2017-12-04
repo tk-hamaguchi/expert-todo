39e8d2e4d01cfe93ed8aea337de4e6cfde913b1e
Takahiro HAMAGUCHI <tk.hamaguchi@gmail.com>
2017-12-04 23:48:03 +0900

    Added config for failed screen-shot with ENV['CIRCLE_ARTIFACTS'].

---
+++ spec/support/screenshot_ext.rb
@@ -0,0 +1,12 @@
+if ENV['CIRCLE_ARTIFACTS']
+  require 'action_dispatch/system_testing/test_helpers/screenshot_helper'
+
+  module ActionDispatch::SystemTesting::TestHelpers::ScreenshotHelper
+
+    private
+
+    def absolute_image_path
+      File.join(ENV['CIRCLE_ARTIFACTS'], 'failed_screenshots', "#{image_name}.png")
+    end
+  end
+end
